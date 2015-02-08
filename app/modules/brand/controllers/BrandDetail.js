b1413e33cf8923fb3febb1db1fe64dfff8ec783d
hdh <hdh@jihui88.com>
2015-02-08 12:50:11 +0800

    hdh

--- app/modules/brand/controllers/BrandDetail.js
+++ app/modules/brand/controllers/BrandDetail.js
@@ -3,8 +3,15 @@
  * @class BrandDetail
  * @author yongjin<zjut_wyj@163.com> 2015/2/8
  */
-define('BrandDetail', [], function (require, exports, module) {
-  var BrandDetail;
+define('BrandDetail', ['App', 'template/brand_detail'], function (require, exports, module) {
+  var BrandDetail, App, template;
+
+  App = require('App');
+  template = require('template/brand_detail');
+
+  BrandDetail = function (page, context) {
+
+  }
 
   module.exports = BrandDetail;
 });
