cf70d4ee902ba0aca75e1b5dfd88d1e504f7ecae
wyj <wyj@abbcc.net>
2015-02-08 10:43:16 +0800

    wyj

--- app/modules/login/controllers/Login.js
+++ app/modules/login/controllers/Login.js
@@ -3,8 +3,19 @@
  * @class Login
  * @author yongjin<zjut_wyj@163.com> 2015/2/8
  */
-define('Login', [], function (require, exports, module) {
-  var Login;
+define('Login', ['App', 'template/login'], function (require, exports, module) {
+  var Login, App, template;
+
+  App = require('App');
+  template = require('template/login');
+
+  Login = function (page, ctx) {
+    $(page).html(template);
+    $(page).find('.btn-back').click(function () {
+      App.back('home', function () {
+      });
+    });
+  }
 
   module.exports = Login;
 });
--- app/modules/login/main.js
+++ app/modules/login/main.js
@@ -3,3 +3,7 @@
  * @class main
  * @author yongjin<zjut_wyj@163.com> 2015/2/8
  */
+App.addModule('Login', 'modules/login/controllers/Login.js');
+App.addTemplate('template/login', function (require, exports, module) {
+  module.exports = require('modules/login/views/login.html');
+});
