1e3bf826ae0d006f5ea01639b0a6089dfc2f3869
hzp <hzp@jihui88.com>
2015-02-09 18:54:47 +0800

    1111

--- app/modules/brand/controllers/IncludeHeader.js
+++ app/modules/brand/controllers/IncludeHeader.js
@@ -6,12 +6,13 @@
 define('IncludeHeader', ['App', 'template/include_header', 'HandlebarsHelper'], function (require, exports, module) {
   var IncludeHeader, App, template, HandlebarsHelper;
 
-    var tpl = HandlebarsHelper.compile(template);
+
       App = require('App');
       template = require('template/include_header');
       HandlebarsHelper = require('HandlebarsHelper');
 
       IncludeHeader = function (page, render, data) {
+        var tpl = HandlebarsHelper.compile(template);
     $(page).find(render).html(tpl(data));
     $(page).find('.nav ul li').click(function () {
       App.load($(this).attr('data-target'), {
