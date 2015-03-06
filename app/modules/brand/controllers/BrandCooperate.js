bb2f77594d35ab1ebb7cfa2ccd04966c03e44f77
hdh <hdh@jihui88.com>
2015-03-06 20:22:21 +0800

    hdh

--- app/modules/brand/controllers/BrandCooperate.js
+++ app/modules/brand/controllers/BrandCooperate.js
@@ -3,14 +3,18 @@
  * @class BrandCooperate
  * @author yongjin<zjut_wyj@163.com> 2015/3/6
  */
-define('BrandCooperate', ['App', 'HandlebarsHelper'], function (require, exports, module) {
-  var BrandCooprate, App, HandlebarsHelper;
+define('BrandCooperate', ['App', 'HandlebarsHelper', 'template/brand_cooperate'], function (require, exports, module) {
+  var BrandCooprate, App, HandlebarsHelper, template;
 
   App = require('App');
   HandlebarsHelper = require('HandlebarsHelper');
+  template = require('template/brand_cooperate');
 
   BrandCooperate = function (page, id) {
-
+    setTimeout(function(){
+      var tpl = HandlebarsHelper.compile(template);
+      $(page).html(tpl({}));
+    },0);
   }
 
   module.exports = BrandCooperate;
